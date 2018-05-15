735f42f6f206111f9960a7b93c77fc5ec98220cd
Kalen Hammann <kalen7@gmail.com>
2018-05-15 17:41:14 -0400

    Restart app from ground zero

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App, { color, number } from './components/app'
+
+
+console.log(color, number);
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
