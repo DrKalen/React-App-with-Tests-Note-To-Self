d955d5a71c265a7c4a300fd127fbd92bba6dcb50
Kalen Hammann <kalen7@gmail.com>
2018-05-18 12:11:44 -0400

    Add Note component

---
+++ src/components/Note.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Note extends Component {
+    render() {
+        return(
+            <div>
+                <p>{this.props.note.text}</p>
+            </div>
+        )
+    }
+}
+export default Note;
